fdb8e1e00cbd9684ef7419d7c56122e6d7005b9c
Matthew <09malee1s@gmail.com>
2012-04-18 19:19:41 -1000

    adjusted Character layout and controller

--- ZombiesAndHumans/src/com/androidbeef/zombiesandhumans/CharacterController.java
+++ ZombiesAndHumans/src/com/androidbeef/zombiesandhumans/CharacterController.java
@@ -4,6 +4,7 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
+import android.widget.Button;
 
 public class CharacterController extends Activity implements OnClickListener
 {
@@ -11,11 +12,15 @@ public class CharacterController extends Activity implements OnClickListener
 	{
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.character_layout);
+		
+		((Button) findViewById(R.id.healthMinus)).setOnClickListener(this);
 	}
-
 	public void onClick(View v)
 	{
-		
+		if(v.getId()==R.id.healthMinus)
+		{
+			
+		}
 	}
 
 }
