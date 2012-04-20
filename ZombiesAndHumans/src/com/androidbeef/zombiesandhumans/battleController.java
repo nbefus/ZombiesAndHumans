31e231758610e1a4248f58ce6f86393a5d43bc92
Matthew <09malee1s@gmail.com>
2012-04-19 17:51:16 -1000

    added some details to the battleController

--- ZombiesAndHumans/src/com/androidbeef/zombiesandhumans/battleController.java
+++ ZombiesAndHumans/src/com/androidbeef/zombiesandhumans/battleController.java
@@ -1,6 +1,26 @@
 package com.androidbeef.zombiesandhumans;
 
-public class battleController
-{
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.TextView;
 
+public class battleController extends Activity
+{
+	protected void onCreate(Bundle savedInstanceState) 
+	{
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.battle);
+		
+		((Button) findViewById(R.id.attackButton)).setOnClickListener(this);
+	}
+	public void onClick(View v)
+	{
+		if(v.getId()==R.id.attackButton)
+		{
+			
+		}
+	}
 }
