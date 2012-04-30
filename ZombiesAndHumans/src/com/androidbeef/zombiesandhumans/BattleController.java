c70a8fda3e1acccf0c98b53071217a2af5850c0a
Matthew <09malee1s@gmail.com>
2012-04-29 19:33:35 -1000

    Started work on the battling between static users.

--- ZombiesAndHumans/src/com/androidbeef/zombiesandhumans/BattleController.java
+++ ZombiesAndHumans/src/com/androidbeef/zombiesandhumans/BattleController.java
@@ -17,6 +17,18 @@ public class BattleController extends Activity implements OnClickListener
 	private TextView uNameDisplay;
 	private TextView uLevelDisplay;
 	private TextView uCurrentIDisplay;
+	private Button attack;
+	private Button retreat;
+	private Button items;
+	
+	private int userLevel=1;
+	private int enemyLevel=1;
+	private int userHealth=100;
+	private int enemyHealth=100;
+	private int userBP=5;
+	private int enemyBP=5;
+	private String userItem="bat";
+	private String enemyItem="none";
 	
 	protected void onCreate(Bundle savedInstanceState) 
 	{
@@ -26,15 +38,21 @@ public class BattleController extends Activity implements OnClickListener
 		((Button) findViewById(R.id.attackButton)).setOnClickListener(this);
 		((Button) findViewById(R.id.retreatButton)).setOnClickListener(this);
 		((Button) findViewById(R.id.itemsButton)).setOnClickListener(this);
-		
-		
+		this.getULevelDisplay().setText(""+userLevel);
+		this.getUHealthDisplay().setText(""+userHealth);
+		this.getUCurrentIDisplay().setText(userItem);
+		this.getELevelDisplay().setText(""+enemyLevel);
+		this.getEHealthDisplay().setText(""+enemyHealth);
+		this.getECurrentIDisplay().setText(enemyItem);
 	}
 	public void onClick(View v)
 	{
+		int enemyHealth=0;
 		if(v.getId()==R.id.attackButton)
 		{
 			//get user's attack info and then deal damage to the enemy's health
-			
+			enemyHealth=Integer.parseInt((String) getECurrentIDisplay().getText());
+			this.getEHealthDisplay().setText(""+(enemyHealth-userBP));
 		}
 		else if(v.getId()==R.id.retreatButton)
 		{
@@ -44,6 +62,22 @@ public class BattleController extends Activity implements OnClickListener
 		{
 			
 		}
+	}
+	public Button getAttackButton()
+	{
+		if(attack==null)
+		{
+			attack=(Button) findViewById(R.id.attackButton);
+		}
+		return attack;
+	}
+	public Button getItemsButton()
+	{
+		if(items==null)
+		{
+			items=(Button) findViewById(R.id.itemsButton);
+		}
+		return items;
 	}
 	public TextView getEHealthDisplay()
 	{
